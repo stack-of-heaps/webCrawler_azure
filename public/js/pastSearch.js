b94d50cbccae734e8e589a8c1e94ffdfbafba8bc
Kyle Karthauser <ky3645@assurity.com>
2019-10-16 22:38:05 -0500

    cookie / past search functions

--- public/js/pastSearch.js
+++ public/js/pastSearch.js
@@ -5,7 +5,7 @@ getPastSearches = () => {
     let pastSearchString = "past-search";
     let substringLength = pastSearchString.length;
     let pastSearches = splitCookie.filter(x => {
-       return x.substring(0, substringLength) === pastSearchString;
+        return x.substring(0, substringLength) === pastSearchString;
     });
 
     return pastSearches;
@@ -22,15 +22,18 @@ getNextSearchNumber = () => {
     let pastSearchString = lastSearch.split('=')[0];
     let numRegEx = /\d+/g
     let nextSearchNumber = Number(pastSearchString.match(numRegEx)[0]) + 1;
-    return nextSearchNumber; 
+    return nextSearchNumber;
 }
 
 displayAllCookies = () => {
+
     let allCookies = document.cookie;
     alert(allCookies);
+
 }
 
 setPastSearchCookie = url => {
+
     let pastSearches = getPastSearches();
     let cookieNumber = null;
     if (pastSearches.length > 0) {
@@ -39,25 +42,35 @@ setPastSearchCookie = url => {
     let thisURL = "http://www.google.com";
     let newCookie = `past-search${cookieNumber}=${thisURL}`;
     document.cookie = newCookie;
+
 }
 
 buildPastSearchTable = () => {
+
     let pastSearchesTbody = document.getElementById("past-searches-tbody");
     let pastSearches = getPastSearches();
     let pastSearchURLs = pastSearches.map(x => {
         return x.split('=')[1];
     })
 
-    pastSearchURLs.forEach( url => {
-        let newTR = document.createElement("tr");
-        let newTD = document.createElement("td");
-        let newAHref = document.createElement("a");
-        newAHref.setAttribute('href', url);
-        newAHref.setAttribute('class', 'text-primary');
-        newAHref.innerText = url;
-        newTD.appendChild(newAHref);
-        newTR.appendChild(newTD);
+    pastSearchURLs.forEach(url => {
+        let newTR = buildSearchEntry(url);
         pastSearchesTbody.appendChild(newTR);
     });
+
+}
+
+buildSearchEntry = url => {
+
+    let newTR = document.createElement("tr");
+    let newTD = document.createElement("td");
+    let newAHref = document.createElement("a");
+    newAHref.setAttribute('href', url);
+    newAHref.setAttribute('class', 'text-primary');
+    newAHref.innerText = url;
+    newTD.appendChild(newAHref);
+    newTR.appendChild(newTD);
+
+    return newTR;
 }
 
