da1ed6d8439878dfc60bd2186edec231b208bcb0
stack-of-heaps <37723013+stack-of-heaps@users.noreply.github.com>
2019-11-10 21:35:25 -0600

    Tidying up
    Also bumped up max transfer size to 200 mb (!)

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const WebScraper = require('./lib/webScraper.js');
 const PORT = 3000;
 
+const axios = require('axios');
+const crawler = require('./lib/crawler');
+const dayjs = require('dayjs');
+const depthSearch = require('./lib/depthSearch');
+const myParser = require("body-parser");
 const Mongo = require('./lib/mongo/mongoModule');
 const MongoManager = require('./lib/mongo/mongoManager');
-const axios = require('axios');
-var express = require('express');
-var path = require('path');
-const dayjs = require('dayjs');
-var app = express();
-app.set('port', PORT);
-const { fork } = require('child_process');
+const express = require('express');
+const path = require('path');
+
+const app = express();
 app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(myParser.json({limit: '200mb'}));
+app.use(myParser.urlencoded({limit: '200mb', extended: true}));
+
+app.set('port', PORT);
 
 Mongo.connect()
   .then(() => console.log('Connected to database.'))
@@ -49,10 +52,10 @@ app.post('/search', async (req, res) => {
   let crawlerResult = null;
 
   if (type === 'depth_search') {
-    crawlerResult = await depthSearch(url, depth);
+    crawlerResult = await depthSearch.crawl(url, depth);
   }
   else {
-    crawlerResult = await invokeCrawler(url);
+    crawlerResult = await crawler.invokeCrawler(url);
   }
 
   res.send(crawlerResult);
@@ -128,6 +131,8 @@ app.post('/newDBEntry', async (req, res) => {
 
 app.post('/updateCrawlerData', async (req, res) => {
 
+  const { search_type, search_url, search_depth } = req.body;
+
   let mongoID = req.body._id;
 
   if (mongoID === null) {
@@ -135,12 +140,24 @@ app.post('/updateCrawlerData', async (req, res) => {
     res.sendStatus(400);
   }
 
-  let crawlerResult = await invokeCrawler(req.body.search_url);
+  let crawlerResult;
+
+  if (search_type === 'depth_search') {
+    console.log('search_type = depth_search')
+    crawlerResult = await depthSearch.crawl(search_url, search_depth);
+  }
+  else {
+    crawlerResult = await crawler.invokeCrakwler(search_url);
+  }
+
   let mongoDTO = {
     depth: req.body.search_depth,
     crawlerData: JSON.stringify(crawlerResult),
     date: dayjs().format()
   }
+
+  console.log('updating crawler data: ');
+  console.log(crawlerResult);
 
   const result = MongoManager.updateCrawlerData(mongoID, mongoDTO);
   result.then(data => {
@@ -156,18 +173,6 @@ app.get('/getPastSearch', async (req, res) => {
 
 app.listen(app.get('port'));
 console.log('Express server listening on port ' + PORT);
-
-async function invokeCrawler(url) {
-  let crawler = fork('./lib/crawler.js');
-
-  return new Promise((resolve, reject) => {
-
-    crawler.send({ url: url });
-    crawler.on('message', result => {
-      resolve(result);
-    })
-  })
-}
 
 async function checkURL(url) {
   return new Promise((resolve, reject) => {
@@ -192,85 +197,6 @@ function createMongoDTO(reqBody) {
 
 app.get('/crawlertest', async (req, res) => {
 
-  depthSearch('http://www.google.com', 3);
+  depthSearch.crawl('http://www.google.com', 3);
 
 })
-
-async function depthSearch(rootURL, searchDepth) {
-
-  let depth = 1;
-
-  let rootResult = await invokeCrawler('http://www.google.com');
-  console.log("ROOTRESULT LINKS: ", rootResult);
-
-  //updateRootNode(rootNode, rootResult);
-  let rootNode = createNode(rootResult, rootURL, rootURL, depth);
-
-  let pagesToVisit = getLinkURLs(rootResult.links);
-  let randomLink = getRandomLink(pagesToVisit, []);
-
-  let visitedPages = [rootURL];
-  let pageQueue = [randomLink];
-  let parentQueue = [rootURL];
-
-  //TODO: PREVENT VISITING LINKS TWICE
-  while (depth < searchDepth) {
-    depth++;
-    console.log('NEW DEPTH: ', depth);
-    
-    if (pageQueue.length < 1) {
-      break;
-    }
-
-    let thisParent = parentQueue.pop();
-    let newLink = pageQueue.pop();
-
-    let childResult = await invokeCrawler(newLink);
-    visitedPages.push(newLink);
-
-    let childNode = createNode(childResult, thisParent, newLink, depth);
-    rootNode.children.push(childNode);
-
-    let childLinks = getLinkURLs(childResult.links);
-    let nextLink = getRandomLink(childLinks, visitedPages);
-    console.log(nextLink);
-
-    pageQueue.push(nextLink);
-    parentQueue.push(nextLink);
-  }
-
-  return rootNode;
-}
-
-function getLinkURLs(linkArray) {
-  let justURLs = linkArray.map(url => {
-    return url.url;
-  })
-
-  return justURLs;
-}
-
-function getRandomLink(linkArray, visitedLinks) {
-  let randomIndex = Math.floor(Math.random() * linkArray.length - 1);
-  let randomLink = linkArray[randomIndex];
-  console.log(randomLink);
-  if (visitedLinks.includes(randomLink)) {
-    console.log('found redundant link');
-    linkArray = linkArray.filter(x => x !== randomLink);
-    return getRandomLink(linkArray, visitedLinks);
-  }
-  else {
-    return randomLink;
-  }
-}
-
-function createNode(crawlerResult, parent, url, depth) {
-
-  let newNode = crawlerResult;
-  newNode.depth = depth;
-  newNode.parent = parent;
-  newNode.self = url;
-
-  return newNode;
-
-}
