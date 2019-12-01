b146cdea4a1a433d1d413fd1fc10c31303c47ae6
stack-of-heaps <37723013+stack-of-heaps@users.noreply.github.com>
2019-12-01 17:37:52 -0600

    Updates
    Custom breadth and depth search parsing for sidepanel

--- public/js/partials/sidePanel.js
+++ public/js/partials/sidePanel.js
@@ -14,34 +14,89 @@ function deleteSidePanel() {
     $('#inspect_results').empty()
 }
 
-export function flattenCrawlerData(crawlerData) {
+export function flattenDepthCrawlerData(crawlerData) {
 
     let depthArray = [];
 
     let nodeCopy = Object.assign({}, crawlerData);
     _.omit(nodeCopy, "children");
     depthArray.push(nodeCopy);
-    console.log('depthArray #1: ', depthArray);
 
-    for (let children of crawlerData.children) {
-        grabChildren(depthArray, children);
+    if (crawlerData && crawlerData.children) {
+        for (let children of crawlerData.children) {
+            grabDepthChildren(depthArray, children);
+        }
     }
-
-    console.log('deptharray #2: ', depthArray);
 
     let orderedByDepth = depthArray.sort(orderByDepth);
 
     return orderedByDepth;
 }
 
-function grabChildren(depthArray, child) {
+export function flattenBreadthCrawlerData(crawlerData) {
+
+    let depthArray = [];
+
+    let nodeCopy = Object.assign({}, crawlerData);
+    _.omit(nodeCopy, "children");
+
+
+
+    if (crawlerData && crawlerData.children) {
+        for (let children of crawlerData.children) {
+            grabBreadthChildren(depthArray, children);
+        }
+    }
+
+    let orderedByDepth = depthArray.sort(orderByDepth);
+
+    return orderedByDepth;
+}
+
+function grabDepthChildren(depthArray, child) {
+
+    let objectType = getObjectType(child);
+
+    if (objectType === 'node') {
+        let refCopy = Object.assign({}, child);
+        let childCopy = _.omit(refCopy, "children");
+        depthArray.push(childCopy);
+
+        if (child.children) {
+            for (let children of child.children) {
+                grabDepthChildren(depthArray, children);
+            }
+        }
+    }
+
+    return;
+}
+
+function grabBreadthChildren(depthArray, child) {
     let refCopy = Object.assign({}, child);
     let childCopy = _.omit(refCopy, "children");
-    depthArray.push(childCopy);
+    
+    let nodeDepth = childCopy.depth - 1;
+    if (_.isArray(depthArray[nodeDepth])) {
+        console.log(`depth: ${nodeDepth}, adding to existing array within depthArray `);
+        depthArray[nodeDepth].push(childCopy);
+    }
+
+    else if (_.isObject(depthArray[nodeDepth])) {
+        console.log(`depth: ${nodeDepth}, creating array within depthArray `);
+        let newArray = new Array();
+        newArray.push(depthArray[nodeDepth]);
+        newArray.push(childCopy);
+        depthArray[nodeDepth] = newArray;
+    }
+    else {
+        console.log(`depth: ${nodeDepth}, just adding node `);
+        depthArray.push(childCopy);
+    }
 
     if (child.children) {
         for (let children of child.children) {
-            grabChildren(depthArray, children);
+            grabBreadthChildren(depthArray, children);
         }
     }
 }
@@ -112,11 +167,36 @@ function displayDepthLinks(crawlerArray, event) {
     displayAllLinks(depthNode);
 }
 
-export function populateSidePanel(crawlerData) {
-    let parsedCrawler = JSON.parse(crawlerData);
-    let flattenedCrawler = flattenCrawlerData(parsedCrawler);
+function getObjectType(obj) {
+    if (obj.orientation) {
+        return 'link';
+    }
+    else {
+        return 'node';
+    }
+}
+
+export function populateSidePanel(crawlerData, searchType) {
+    let parsedCrawler = null;
+
+    if (typeof (crawlerData) === 'string') {
+        parsedCrawler = JSON.parse(crawlerData);
+    }
+    else {
+        parsedCrawler = crawlerData;
+    }
+
+    let flattenedCrawler = null;
+    console.log('searchtype: ', searchType);
+    if (searchType === 'depth_search') {
+        flattenedCrawler = flattenDepthCrawlerData(parsedCrawler);
+    }
+    else {
+        flattenedCrawler = flattenBreadthCrawlerData(parsedCrawler);
+    }
     //displayAllLinks(flattenedCrawler, 1);
     buildSidePanelDepth(flattenedCrawler);
+    console.log('flattenedCrawler: ', flattenedCrawler);
 
     document.getElementById('jump-to-depth').addEventListener('change', function (event) { displayDepthLinks(flattenedCrawler, event) });
 }
